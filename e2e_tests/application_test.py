54f5a1e75610344272df8ac1dfd64a20b69e9b35
axshani <aalexshani@gmail.com>
2021-08-21 19:43:20 +0300

    small fix

--- e2e_tests/application_test.py
+++ e2e_tests/application_test.py
@@ -57,8 +57,6 @@ class ApplicationE2eTests(unittest.TestCase):
         token = os.environ.get("token")
         client = Unit("https://api.s.unit.sh", token)
         response = client.applications.list_documents("61176")
-        x = 5
-
 
 if __name__ == '__main__':
     unittest.main()
