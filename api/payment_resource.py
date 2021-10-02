84551c46243f4aa8020f13cd2827648b09e07522
axshani <aalexshani@gmail.com>
2021-10-02 15:46:37 +0300

    commit payments

--- api/payment_resource.py
+++ api/payment_resource.py
@@ -8,8 +8,26 @@ class PaymentResource(BaseResource):
         super().__init__(api_url, token)
         self.resource = "payments"
 
-    def get(self, payment_id: str) -> Union[UnitResponse[PaymentDTO], UnitError]:
-        response = super().get(f"{self.resource}/{payment_id}")
+    def create(self, request: CreatePaymentRequest) -> Union[UnitResponse[PaymentDTO], UnitError]:
+        payload = request.to_json_api()
+        response = super().post(self.resource, payload)
+        if super().is_20x(response.status_code):
+            data = response.json().get("data")
+            return UnitResponse[PaymentDTO](DtoDecoder.decode(data), None)
+        else:
+            return UnitError.from_json_api(response.json())
+
+    def update(self, request: PatchPaymentRequest) -> Union[UnitResponse[PaymentDTO], UnitError]:
+        payload = request.to_json_api()
+        response = super().patch(f"{self.resource}/{request.payment_id}", payload)
+        if super().is_20x(response.status_code):
+            data = response.json().get("data")
+            return UnitResponse[PaymentDTO](DtoDecoder.decode(data), None)
+        else:
+            return UnitError.from_json_api(response.json())
+
+    def get(self, payment_id: str, include: Optional[str] = "") -> Union[UnitResponse[PaymentDTO], UnitError]:
+        response = super().get(f"{self.resource}/{payment_id}", {"include": include})
         if response.status_code == 200:
             data = response.json().get("data")
             included = response.json().get("included")
--- models/payment.py
+++ models/payment.py
@@ -74,3 +74,161 @@ class WirePaymentDTO(BasePayment):
 
 PaymentDTO = Union[AchPaymentDTO, BookPaymentDTO, WirePaymentDTO]
 
+class CreatePaymentRequest(object):
+    def __init__(self, amount: int, description: str, relationships: dict[str, Relationship],
+                 idempotency_key: Optional[str], tags: Optional[dict[str, str]], direction: str = "Credit",
+                 type: str = "achPayment"):
+        self.type = type
+        self.amount = amount
+        self.description = description
+        self.direction = direction
+        self.idempotency_key = idempotency_key
+        self.tags = tags
+        self.relationships = relationships
+
+    def to_json_api(self) -> dict:
+        payload = {
+            "data": {
+                "type": self.type,
+                "attributes": {
+                    "amount": self.amount,
+                    "direction": self.direction,
+                    "description": self.description
+                },
+                "relationships": self.relationships
+            }
+        }
+
+        if self.idempotency_key:
+            payload["data"]["attributes"]["idempotencyKey"] = self.idempotency_key
+
+        if self.tags:
+            payload["data"]["attributes"]["tags"] = self.tags
+
+        return payload
+
+    def __repr__(self):
+        json.dumps(self.to_json_api())
+
+class CreateInlinePaymentRequest(CreatePaymentRequest):
+    def __init__(self, amount: int, description: str, counterparty: Counterparty, relationships: dict[str, Relationship],
+                 addenda: Optional[str], idempotency_key: Optional[str], tags: Optional[dict[str, str]],
+                 direction: str = "Credit"):
+        CreatePaymentRequest.__init__(self, amount, description, relationships, idempotency_key, tags, direction)
+        self.counterparty = counterparty
+        self.addenda = addenda
+
+    def to_json_api(self) -> dict:
+        payload = CreatePaymentRequest.to_json_api(self)
+        payload["data"]["attributes"]["counterparty"] = self.counterparty
+
+        if self.addenda:
+            payload["data"]["attributes"]["addenda"] = self.addenda
+
+        return payload
+
+class CreateLinkedPaymentRequest(CreatePaymentRequest):
+    def __init__(self, amount: int, description: str, relationships: dict[str, Relationship], addenda: Optional[str],
+                 verify_counterparty_balance: Optional[bool], idempotency_key: Optional[str],
+                 tags: Optional[dict[str, str]], direction: str = "Credit"):
+        CreatePaymentRequest.__init__(self, amount, description, relationships, idempotency_key, tags, direction)
+        self.addenda = addenda
+        self.verify_counterparty_balance = verify_counterparty_balance
+
+    def to_json_api(self) -> dict:
+        payload = CreatePaymentRequest.to_json_api(self)
+
+        if self.addenda:
+            payload["data"]["attributes"]["addenda"] = self.addenda
+
+        if self.verify_counterparty_balance:
+            payload["data"]["attributes"]["verifyCounterpartyBalance"] = self.verify_counterparty_balance
+
+        return payload
+
+class CreateVerifiedPaymentRequest(CreatePaymentRequest):
+    def __init__(self, amount: int, description: str, plaid_processor_token: str, relationships: dict[str, Relationship],
+                 counterparty_name: Optional[str], verify_counterparty_balance: Optional[bool],
+                 idempotency_key: Optional[str], tags: Optional[dict[str, str]], direction: str = "Credit"):
+        CreatePaymentRequest.__init__(self, amount, description, relationships, idempotency_key, tags)
+        self.plaid_Processor_token = plaid_Processor_token
+        self.counterparty_name = counterparty_name
+        self.verify_counterparty_balance = verify_counterparty_balance
+
+    def to_json_api(self) -> dict:
+        payload = CreatePaymentRequest.to_json_api(self)
+        payload["data"]["attributes"]["counterparty"] = self.counterparty
+        payload["data"]["attributes"]["plaidProcessorToken"] = self.plaid_processor_token
+
+        if counterparty_name:
+            payload["data"]["attributes"]["counterpartyName"] = self.counterparty_name
+
+        if verify_counterparty_balance:
+            payload["data"]["attributes"]["verifyCounterpartyBalance"] = self.verify_counterparty_balance
+
+
+        return payload
+
+class CreateBookPaymentRequest(CreatePaymentRequest):
+    def __init__(self, amount: int, description: str, relationships: dict[str, Relationship],
+                 idempotency_key: Optional[str] = None, tags: Optional[dict[str, str]] = None,
+                 direction: str = "Credit"):
+        super().__init__(amount, description, relationships, idempotency_key, tags, direction, "bookPayment")
+
+class CreateWirePaymentRequest(CreatePaymentRequest):
+    def __init__(self, amount: int, description: str, counterparty: WireCounterparty,
+                 relationships: dict[str, Relationship], idempotency_key: Optional[str], tags: Optional[dict[str, str]],
+                 direction: str = "Credit"):
+        CreatePaymentRequest.__init__(self, amount, description, relationships, idempotency_key, tags, direction,
+                                      "wirePayment")
+        self.counterparty = counterparty
+
+    def to_json_api(self) -> dict:
+        payload = CreatePaymentRequest.to_json_api(self)
+        payload["data"]["attributes"]["counterparty"] = self.counterparty
+        return payload
+
+CreatePaymentRequest = Union[CreateInlinePaymentRequest, CreateLinkedPaymentRequest, CreateVerifiedPaymentRequest,
+                             CreateBookPaymentRequest, CreateWirePaymentRequest]
+
+class PatchAchPaymentRequest(object):
+    def __init__(self, payment_id: str, tags: dict[str, str]):
+        self.payment_id = payment_id
+        self.tags = tags
+
+    def to_json_api(self) -> dict:
+        payload = {
+            "data": {
+                "type": "achPayment",
+                "attributes": {
+                    "tags": self.tags
+                }
+            }
+        }
+
+        return payload
+
+    def __repr__(self):
+        json.dumps(self.to_json_api())
+
+class PatchBookPaymentRequest(object):
+    def __init__(self, payment_id: str, tags: dict[str, str]):
+        self.payment_id = payment_id
+        self.tags = tags
+
+    def to_json_api(self) -> dict:
+        payload = {
+            "data": {
+                "type": "bookPayment",
+                "attributes": {
+                    "tags": self.tags
+                }
+            }
+        }
+
+        return payload
+
+    def __repr__(self):
+        json.dumps(self.to_json_api())
+
+PatchPaymentRequest = Union[PatchAchPaymentRequest, PatchBookPaymentRequest]
