6bbf8116a3c6abceb20ab141262ac41b2eec8ccc
axshani <aalexshani@gmail.com>
2021-10-11 13:31:37 +0300

    implemented applicationForms resource with tests

---
+++ e2e_tests/applicationForm_test.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+from api.unit import Unit
+from models.applicationForm import CreateApplicationFormRequest
+
+
+class ApplicationFormE2eTests(unittest.TestCase):
+    token = os.environ.get("token")
+    client = Unit("https://api.s.unit.sh", token)
+    card_types = ["individualDebitCard", "businessDebitCard", "individualVirtualDebitCard", "businessVirtualDebitCard"]
+
+    def create_create_application_form(self):
+        request = CreateApplicationFormRequest(tags={"userId": "106a75e9-de77-4e25-9561-faffe59d7814"},
+                                               allowed_application_types=["Individual"])
+        return self.client.applicationForm.create(request)
+
+    def test_create_application_form(self):
+        response = self.create_create_application_form()
+        self.assertTrue(response.data.type == "applicationForm")
+
+    def test_get_application_form(self):
+        application_form_id = self.create_create_application_form().data.id
+        response = self.client.applicationForm.get(application_form_id)
+        self.assertTrue(response.data.type == "applicationForm")
+
+    def test_list_application_form(self):
+        response = self.client.applicationForm.list()
+        for app in response.data:
+            self.assertTrue(app.type == "applicationForm")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
--- models/codecs.py
+++ models/codecs.py
@@ -8,6 +8,7 @@ from models.customer import IndividualCustomerDTO, BusinessCustomerDTO
 from models.card import IndividualDebitCardDTO, BusinessDebitCardDTO, IndividualVirtualDebitCardDTO, BusinessVirtualDebitCardDTO
 from models.transaction import *
 from models.payment import AchPaymentDTO, BookPaymentDTO, WirePaymentDTO
+from models.applicationForm import ApplicationFormDTO
 
 mappings = {
         "individualApplication": lambda _id, _type, attributes, relationships:
@@ -102,6 +103,9 @@ mappings = {
 
         "wirePayment": lambda _id, _type, attributes, relationships:
         WirePaymentDTO.from_json_api(_id, _type, attributes, relationships),
+
+        "applicationForm": lambda _id, _type, attributes, relationships:
+        ApplicationFormDTO.from_json_api(_id, _type, attributes, relationships),
 
     }
 
