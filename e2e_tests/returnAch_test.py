cd61e01675afdc68bb31480c5882bcc260b59227
axshani <aalexshani@gmail.com>
2021-10-18 10:38:12 +0300

    payload small fix

--- e2e_tests/returnAch_test.py
+++ e2e_tests/returnAch_test.py
@@ -1,8 +1,8 @@
 import os
 import unittest
 from api.unit import Unit
-from e2e_tests.transaction_test import TransactionE2eTests
-from models.returnAch import ReturnReceivedAchTransactionRequest
+from models.returnAch import *
+
 
 class ReturnAchE2eTests(unittest.TestCase):
     token = os.environ.get("token")
@@ -12,13 +12,16 @@ class ReturnAchE2eTests(unittest.TestCase):
         response = self.client.transactions.list()
         for t in response.data:
             if t.type == "receivedAchTransaction":
-                return t.id
+                return t
 
         return ""
 
     def test_return_ach(self):
-        transaction_id = get_received_ach_transaction()
-        request = ReturnReceivedAchTransactionRequest(transaction_id, "Unauthorized")
+        transaction = self.get_received_ach_transaction()
+        transaction_id = transaction.id
+        account_id = transaction.relationships["account"].id
+        request = ReturnReceivedAchTransactionRequest(transaction_id, "Unauthorized",
+                                                      {"account": Relationship("depositAccount", account_id)})
         response = self.client.returnAch.return_ach(request)
         self.assertTrue(response.data.type == "returnedReceivedAchTransaction")
 
--- models/returnAch.py
+++ models/returnAch.py
@@ -6,9 +6,10 @@ AchReturnReason = Literal["Unauthorized"]
 
 
 class ReturnReceivedAchTransactionRequest(object):
-    def __init__(self, transaction_id: str, reason: AchReturnReason):
+    def __init__(self, transaction_id: str, reason: AchReturnReason, relationships: [dict[str, Relationship]]):
         self.transaction_id = transaction_id
         self.reason = reason
+        self.relationships = relationships
 
     def to_json_api(self) -> dict:
         payload = {
@@ -16,7 +17,8 @@ class ReturnReceivedAchTransactionRequest(object):
                 "type": "returnAch",
                 "attributes": {
                     "reason": self.reason
-                }
+                },
+                "relationships": self.relationships
             }
         }
 
