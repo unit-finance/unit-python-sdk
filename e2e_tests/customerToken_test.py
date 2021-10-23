955ddc2d7a693f14240d717c0cf04bfc756dedda
axshani <aalexshani@gmail.com>
2021-10-23 13:00:21 +0300

    create customer

--- e2e_tests/customerToken_test.py
+++ e2e_tests/customerToken_test.py
@@ -2,19 +2,23 @@ import os
 import unittest
 from api.unit import Unit
 from models.customerToken import *
+from e2e_tests.account_test import AccountE2eTests
 
 
 class CustomerTokenE2eTests(unittest.TestCase):
     token = os.environ.get("token")
     client = Unit("https://api.s.unit.sh", token)
+    accountTests = AccountE2eTests()
 
     def test_create_token(self):
-        request = CreateCustomerToken("101217", "customers accounts")
+        account_id = self.accountTests.create_individual_customer()
+        request = CreateCustomerToken(account_id, "customers accounts")
         response = self.client.customerTokens.create_token(request)
         self.assertTrue(response.data.type == "customerBearerToken")
 
     def test_create_token_verification(self):
-        request = CreateCustomerTokenVerification("101217", "sms")
+        account_id = self.accountTests.create_individual_customer()
+        request = CreateCustomerTokenVerification(account_id, "sms")
         response = self.client.customerTokens.create_token_verification(request)
         self.assertTrue(response.data.type == "customerTokenVerification")
 
