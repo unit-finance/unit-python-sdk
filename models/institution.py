42004728b65a9852f70a08efff70184a558f9fb6
axshani <aalexshani@gmail.com>
2021-10-30 23:32:30 +0300

    institutions completed

--- models/institution.py
+++ models/institution.py
@@ -5,8 +5,13 @@ from models import *
 
 class InstitutionDTO(object):
     def __init__(self, routing_number: str, name: str, is_ach_supported: bool, is_wire_supported: bool,
-                 ddress: Optional[Address] = None):
+                 address: Optional[Address] = None):
         self.type = "institution"
         self.attributes = {"routingNumber": routing_number, "name": name, "address": address,
                            "isACHSupported": is_ach_supported, "isWireSupported": is_wire_supported}
+
+    def from_json_api(_id, _type, attributes, relationships):
+        return InstitutionDTO(
+            attributes["routingNumber"], attributes["name"], attributes["isACHSupported"], attributes["isWireSupported"],
+            attributes.get("address"))
 
