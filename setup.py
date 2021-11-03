209b9f055887129ea7ecd520d624c63d2be99fe3
Alex Nickolaevsky <alex@unit.co>
2021-11-03 15:16:29 +0200

    pypi deployment files

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(
     name='unit-python-sdk',
-    packages=['api'],
-    version='0.1',
+    packages=['api', 'models', 'utils'],
+    version='0.2',
     license='Mozilla Public License 2.0',
     description='This library provides a python wrapper to http://unit.co API. See https://docs.unit.co/',
     author='unit.co',
