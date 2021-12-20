417ac94af48bf687d417f9194c3ce8baff935261
axshani <aalexshani@gmail.com>
2021-12-20 12:20:33 +0200

    init pytest

--- e2e_tests/event_test.py
+++ e2e_tests/event_test.py
@@ -1,29 +1,24 @@
 import os
-import unittest
+import pytest
 from datetime import datetime, date, timedelta
 from unit import Unit
-from unit.models.event import *
 
-class EventE2eTests(unittest.TestCase):
-    token = os.environ.get("token")
-    client = Unit("https://api.s.unit.sh", token)
-
-    def test_list_and_get_events(self):
-        event_ids = []
-        response = self.client.events.list()
-        for e in response.data:
-            self.assertTrue("." in e.type)
-            event_ids.append(e.id)
-
-        for e in event_ids:
-            response = self.client.events.get(e)
-            self.assertTrue("." in response.data.type)
-
-    def test_fire_event(self):
-        event_id = self.client.events.list().data[0].id
-        response = self.client.events.fire(event_id)
-        self.assertTrue(response.data == [])
+token = os.environ.get("token")
+client = Unit("https://api.s.unit.sh", token)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_list_and_get_events(self):
+    event_ids = []
+    response = self.client.events.list()
+    for e in response.data:
+        self.assertTrue("." in e.type)
+        event_ids.append(e.id)
+
+    for e in event_ids:
+        response = self.client.events.get(e)
+        self.assertTrue("." in response.data.type)
+
+def test_fire_event(self):
+    event_id = self.client.events.list().data[0].id
+    response = self.client.events.fire(event_id)
+    self.assertTrue(response.data == [])
