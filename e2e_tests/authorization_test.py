b65a61e9a4897de33601feefe4c6a45788143ee3
axshani <aalexshani@gmail.com>
2022-02-14 13:25:07 +0200

    done with authorization added sort to list params added status to authorization added tests

--- e2e_tests/authorization_test.py
+++ e2e_tests/authorization_test.py
@@ -14,15 +14,31 @@ class AuthorizationsE2eTests(unittest.TestCase):
             response = self.client.authorizations.get(authorization.id, True)
             self.assertTrue(response.data.type == "authorization")
 
+    def test_list_and_get_with_filter_by_status(self):
+        params = ListAuthorizationParams(status="Authorized")
+        authorizations = self.client.authorizations.list(params)
+
+        for authorization in authorizations.data:
+            response = self.client.authorizations.get(authorization.id)
+            self.assertTrue(response.data.attributes["status"] == "Authorized")
+
+    def test_list_with_non_authorized(self):
+        params = ListAuthorizationParams(include_non_authorized=True)
+        authorizations = self.client.authorizations.list(params)
+
+        for authorization in authorizations.data:
+            response = self.client.authorizations.get(authorization.id)
+            self.assertTrue(response.data.attributes["status"] == "Authorized")
+
     def test_list_with_parameters(self):
-        params = AuthorizationListParams(10, 0, "", "49423")
+        params = ListAuthorizationParams(10, 0, "", "49423")
         authorizations = self.client.authorizations.list(params)
         for authorization in authorizations.data:
             response = self.client.authorizations.get(authorization.id)
             self.assertTrue(response.data.type == "authorization")
 
     def test_list_with_wrong_parameters(self):
-        params = AuthorizationListParams(10, 0, "", "-1", include_non_authorized=False)
+        params = ListAuthorizationParams(10, 0, "", "-1", include_non_authorized=False)
         response = self.client.authorizations.list(params)
         self.assertTrue(response.data == [])
 
--- unit/models/authorization.py
+++ unit/models/authorization.py
@@ -3,22 +3,25 @@ from typing import Optional
 from unit.models import *
 from unit.utils import date_utils
 
+AuthorizationStatus = Literal["Authorized", "Completed", "Canceled", "Declined"]
 
 class AuthorizationDTO(object):
-    def __init__(self, id: str, created_at: datetime, amount: int, card_last_4_digits: str, merchant_name: str,
+    def __init__(self, id: str, created_at: datetime, amount: int, card_last_4_digits: str, status: AuthorizationStatus,
+                 merchant_name: str,
                  merchant_type: int, merchant_category: str, merchant_location: Optional[str], recurring: bool,
                  tags: Optional[Dict[str, str]], relationships: Optional[Dict[str, Relationship]]):
         self.id = id
         self.type = "authorization"
         self.attributes = {"createdAt": created_at, "amount": amount, "cardLast4Digits": card_last_4_digits,
-                           "merchant": { "name": merchant_name, "type": merchant_type, "category": merchant_category,
-                                         "location": merchant_location}, "recurring": recurring, "tags": tags}
+                           "status": status, "merchant": {"name": merchant_name, "type": merchant_type,
+                                                          "category": merchant_category, "location": merchant_location},
+                           "recurring": recurring, "tags": tags}
         self.relationships = relationships
 
     @staticmethod
     def from_json_api(_id, _type, attributes, relationships):
         return AuthorizationDTO(_id, date_utils.to_datetime(attributes["createdAt"]), attributes["amount"],
-                                attributes["cardLast4Digits"], attributes["merchant"]["name"],
+                                attributes["cardLast4Digits"], attributes["status"], attributes["merchant"]["name"],
                                 attributes["merchant"]["type"], attributes["merchant"]["category"],
                                 attributes["merchant"].get("location"), attributes["recurring"],
                                 attributes.get("tags"), relationships)
@@ -28,7 +31,7 @@ class ListAuthorizationParams(UnitParams):
     def __init__(self, limit: int = 100, offset: int = 0, account_id: Optional[str] = None,
                  customer_id: Optional[str] = None, card_id: Optional[str] = None, since: Optional[str] = None,
                  until: Optional[str] = None, include_non_authorized: Optional[bool] = False,
-                 status: Optional[str] = None):
+                 status: Optional[str] = None, sort: Optional[str] = "-createdAt"):
         self.limit = limit
         self.offset = offset
         self.account_id = account_id
@@ -38,9 +41,10 @@ class ListAuthorizationParams(UnitParams):
         self.until = until
         self.include_non_authorized = include_non_authorized
         self.status = status
+        self.sort = sort
 
     def to_dict(self) -> Dict:
-        parameters = {"page[limit]": self.limit, "page[offset]": self.offset}
+        parameters = {"page[limit]": self.limit, "page[offset]": self.offset, "sort": self.sort}
         if self.customer_id:
             parameters["filter[customerId]"] = self.customer_id
         if self.account_id:
