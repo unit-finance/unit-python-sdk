01daef4bfee09b4274ca697d71fd76d8c212d733
Asaf <asaf@unit.co>
2022-03-23 10:46:03 +0200

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='unit-python-sdk',
     packages=['unit', 'unit.api', 'unit.models', 'unit.utils'],
-    version='0.9.3',
+    version='0.10.3',
     license='Mozilla Public License 2.0',
     description='This library provides a python wrapper to http://unit.co API. See https://docs.unit.co/',
     author='unit.co',
