71bc2747f5a78b6d9aaf5c89aa62c92c745f22e1
axshani <aalexshani@gmail.com>
2022-05-06 18:23:37 +0300

    added code

--- unit/models/__init__.py
+++ unit/models/__init__.py
@@ -46,12 +46,13 @@ class RawUnitObject(object):
 
 class UnitErrorPayload(object):
     def __init__(self, title: str, status: str, detail: Optional[str] = None, details: Optional[str] = None,
-                 source: Optional[Dict] = None):
+                 source: Optional[Dict] = None, code: Optional[str] = None):
         self.title = title
         self.status = status
         self.detail = detail
         self.details = details
         self.source = source
+        self.code = code
 
     def __str__(self):
         return self.detail
@@ -67,7 +68,7 @@ class UnitError(object):
         for err in data["errors"]:
             errors.append(
                 UnitErrorPayload(err.get("title"), err.get("status"), err.get("detail", None),
-                                 err.get("details", None), err.get("source", None))
+                                 err.get("details", None), err.get("source", None), err.get("code", None))
             )
 
         return UnitError(errors)
