b17f1292ff2fc55cd4bb5be0d715619ac7162a7b
chita13 <reut@unit.co>
2022-05-11 12:03:38 +0300

    Fix api token expiration (#110)
    * fix api token expiration
    
    * fix
    
    * backward compatibility

--- unit/models/api_token.py
+++ unit/models/api_token.py
@@ -20,10 +20,12 @@ class APITokenDTO(object):
 class CreateAPITokenRequest(object):
     def __init__(self, user_id: str, description: str, scope: str, expiration: datetime,
                  source_ip: Optional[str] = None):
+        # for backward compatibility
+        expiration_dt: str = expiration if isinstance(expiration, str) else date_utils.from_datetime(expiration)
         self.user_id = user_id
         self.description = description
         self.scope = scope
-        self.expiration = expiration
+        self.expiration = expiration_dt
         self.source_ip = source_ip
 
     def to_json_api(self) -> Dict:
--- unit/utils/date_utils.py
+++ unit/utils/date_utils.py
@@ -4,6 +4,8 @@ from datetime import date, datetime
 def to_datetime(dt: str):
     return datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S.%f%z")
 
+def from_datetime(dt: datetime):
+    return dt.isoformat(timespec='milliseconds') + "Z"
 
 def to_date(d: str):
     return date.fromisoformat(d)
