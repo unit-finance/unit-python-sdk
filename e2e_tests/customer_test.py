575c95e3049d9489819c2623e53741457a042902
Alex Shani <77099403+axshani@users.noreply.github.com>
2022-05-19 13:14:35 +0300

    New fields to Customers (#121)
    * new fields to Customers
    fix to AuthorizedUser from json api
    
    * typo
    
    * jwtSubject added
    
    * added

--- e2e_tests/customer_test.py
+++ e2e_tests/customer_test.py
@@ -2,6 +2,7 @@ import os
 import unittest
 from unit import Unit
 from unit.models.customer import *
+from unit.models.codecs import DtoDecoder
 
 token = os.environ.get('TOKEN')
 client = Unit("https://api.s.unit.sh", token)
@@ -39,3 +40,170 @@ def test_list_customers():
     response = client.customers.list()
     for customer in response.data:
         assert customer.type == "individualCustomer" or customer.type == "businessCustomer"
+
+
+def test_business_customer():
+    business_customer_api_response = {
+          "type": "businessCustomer",
+          "id": "1",
+          "attributes": {
+            "createdAt": "2020-05-10T12:28:37.698Z",
+            "name": "Pied Piper",
+            "address": {
+              "street": "5230 Newell Rd",
+              "street2": None,
+              "city": "Palo Alto",
+              "state": "CA",
+              "postalCode": "94303",
+              "country": "US"
+            },
+            "phone": {
+              "countryCode": "1",
+              "number": "1555555578"
+            },
+            "stateOfIncorporation": "DE",
+            "ein": "123456789",
+            "entityType": "Corporation",
+            "contact": {
+              "fullName": {
+                "first": "Richard",
+                "last": "Hendricks"
+              },
+              "email": "richard@piedpiper.com",
+              "phone": {
+                "countryCode": "1",
+                "number": "1555555578"
+              }
+            },
+            "authorizedUsers": [
+              {
+                "fullName": {
+                  "first": "Jared",
+                  "last": "Dunn"
+                },
+                "email": "jared@piedpiper.com",
+                "phone": {
+                  "countryCode": "1",
+                  "number": "1555555590"
+                }
+              },
+                {
+                    "fullName": {
+                        "first": "Jared",
+                        "last": "Dunn"
+                    },
+                    "email": "jared@piedpiper.com",
+                    "phone": {
+                        "countryCode": "1",
+                        "number": "1555555590"
+                    }
+                },{
+                "fullName": {
+                  "first": "Jared",
+                  "last": "Dunn"
+                },
+                "email": "jared@piedpiper.com",
+                "phone": {
+                  "countryCode": "1",
+                  "number": "1555555590"
+                }
+              }
+            ],
+            "status": "Active",
+            "tags": {
+              "userId": "106a75e9-de77-4e25-9561-faffe59d7814"
+            }
+          },
+          "relationships": {
+            "org": {
+              "data": {
+                "type": "org",
+                "id": "1"
+              }
+            },
+            "application": {
+              "data": {
+                "type": "businessApplication",
+                "id": "1"
+              }
+            }
+          }
+        }
+
+    id = business_customer_api_response["id"]
+    _type = business_customer_api_response["type"]
+
+    customer = DtoDecoder.decode(business_customer_api_response)
+
+    assert customer.id == id
+    assert customer.type == _type
+    assert customer.attributes["address"].street == "5230 Newell Rd"
+    assert customer.attributes["name"] == "Pied Piper"
+    assert customer.attributes["stateOfIncorporation"] == "DE"
+    assert customer.attributes["contact"].full_name.first == "Richard"
+    assert customer.attributes["authorizedUsers"][0].full_name.first == "Jared"
+    assert customer.attributes["status"] == "Active"
+
+def test_individual_customer():
+    individual_customer_api_response = {
+        "type": "individualCustomer",
+        "id": "8",
+        "attributes": {
+            "createdAt": "2020-05-12T19:41:04.123Z",
+            "fullName": {
+                "first": "Peter",
+                "last": "Parker"
+            },
+            "ssn": "721074426",
+            "address": {
+                "street": "20 Ingram St",
+                "street2": None,
+                "city": "Forest Hills",
+                "state": "NY",
+                "postalCode": "11375",
+                "country": "US"
+            },
+            "dateOfBirth": "2001-08-10",
+            "email": "peter@oscorp.com",
+            "phone": {
+                "countryCode": "1",
+                "number": "1555555578"
+            },
+            "authorizedUsers": [],
+            "status": "Active",
+            "tags": {
+                "userId": "106a75e9-de77-4e25-9561-faffe59d7814"
+            }
+        },
+        "relationships": {
+            "org": {
+                "data": {
+                    "type": "org",
+                    "id": "1"
+                }
+            },
+            "application": {
+                "data": {
+                    "type": "individualApplication",
+                    "id": "8"
+                }
+            }
+        }
+    }
+
+
+    id = individual_customer_api_response["id"]
+    _type = individual_customer_api_response["type"]
+
+    customer = DtoDecoder.decode(individual_customer_api_response)
+
+    assert customer.id == id
+    assert customer.type == _type
+    assert customer.attributes["address"].street == "20 Ingram St"
+    assert customer.attributes["fullName"].first == "Peter"
+    assert customer.attributes["fullName"].last == "Parker"
+    assert customer.attributes["email"] == "peter@oscorp.com"
+    assert customer.attributes["phone"].number == "1555555578"
+    assert customer.attributes["authorizedUsers"] == []
+    assert customer.attributes["status"] == "Active"
+
