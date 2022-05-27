ee7bd0ee16601fcc6cdee310f48a9416ff2a7f30
axshani <aalexshani@gmail.com>
2022-05-27 10:54:47 +0300

    ready for use

--- unit/models/__init__.py
+++ unit/models/__init__.py
@@ -8,14 +8,16 @@ def to_camel_case(snake_str):
     # with the 'title' method and join them together.
     return components[0] + ''.join(x.title() for x in components[1:])
 
-def dto_to_dict(dto):
-    if type(dto) is dict:
-        return dto
-    else:
-        v = vars(dto)
-        return dict((to_camel_case(k), val) for k, val in v.items() if val is not None)
+class UnitDTO(object):
+    def to_dict(self):
+        if type(self) is dict:
+            return dto
+        else:
+            v = vars(self)
+            return dict((to_camel_case(k), val) for k, val in v.items() if val is not None)
 
-class Relationship(object):
+
+class Relationship(UnitDTO):
     def __init__(self, _type: str, _id: str):
         self.type = _type
         self.id = _id
@@ -29,7 +31,7 @@ class Relationship(object):
 
 T = TypeVar('T')
 
-class RelationshipArray(Generic[T]):
+class RelationshipArray(Generic[T], UnitDTO):
     def __init__(self, l: List[T]):
         self.relationships = l
 
@@ -55,12 +57,16 @@ class UnitParams(object):
     def to_dict(self) -> Dict:
         pass
 
-class RawUnitObject(object):
+class RawUnitObject(UnitDTO):
     def __init__(self, _id, _type, attributes, relationships):
         self.id = _id
         self.type = _type
         self.attributes = attributes
         self.relationships = relationships
+
+    def to_dict(self):
+        v = vars(self.attributes)
+        return dict((to_camel_case(k), val) for k, val in v.items() if val is not None)
 
 class UnitErrorPayload(object):
     def __init__(self, title: str, status: str, detail: Optional[str] = None, details: Optional[str] = None,
@@ -100,7 +106,7 @@ Status = Literal["Approved", "Denied", "PendingReview"]
 Title = Literal["CEO", "COO", "CFO", "President"]
 EntityType = Literal["Corporation", "LLC", "Partnership"]
 
-class FullName(object):
+class FullName(UnitDTO):
     def __init__(self, first: str, last: str):
         self.first = first
         self.last = last
@@ -109,12 +115,9 @@ class FullName(object):
     def from_json_api(data: Dict):
         return FullName(data.get("first"), data.get("last"))
 
-    def to_dict(self):
-        return dto_to_dict(self)
-
 
 # todo: Alex - use typing.Literal for multi accepted values (e.g country)
-class Address(object):
+class Address(UnitDTO):
     def __init__(self, street: str, city: str, state: str, postal_code: str, country: str,
                  street2: Optional[str] = None):
         self.street = street
@@ -129,11 +132,8 @@ class Address(object):
         return Address(data.get("street"), data.get("city"), data.get("state"),
                 data.get("postalCode"), data.get("country"), data.get("street2", None))
 
-    def to_dict(self):
-        return dto_to_dict(self)
 
-
-class Phone(object):
+class Phone(UnitDTO):
     def __init__(self, country_code: str, number: str):
         self.country_code = country_code
         self.number = number
@@ -143,7 +143,7 @@ class Phone(object):
         return Phone(data.get("countryCode"), data.get("number"))
 
 
-class BusinessContact(object):
+class BusinessContact(UnitDTO):
     def __init__(self, full_name: FullName, email: str, phone: Phone):
         self.full_name = full_name
         self.email = email
@@ -154,7 +154,7 @@ class BusinessContact(object):
         return BusinessContact(FullName.from_json_api(data.get("fullName")), data.get("email"), Phone.from_json_api(data.get("phone")))
 
 
-class Officer(object):
+class Officer(UnitDTO):
     def __init__(self, full_name: FullName, date_of_birth: date, address: Address, phone: Phone, email: str,
                  status: Optional[Status] = None, title: Optional[Title] = None, ssn: Optional[str] = None,
                  passport: Optional[str] = None, nationality: Optional[str] = None):
@@ -176,7 +176,7 @@ class Officer(object):
                 data.get("nationality"))
 
 
-class BeneficialOwner(object):
+class BeneficialOwner(UnitDTO):
     def __init__(self, full_name: FullName, date_of_birth: date, address: Address, phone: Phone, email: str,
                  status: Optional[Status] = None, ssn: Optional[str] = None, passport: Optional[str] = None,
                  nationality: Optional[str] = None, percentage: Optional[int] = None):
@@ -201,7 +201,7 @@ class BeneficialOwner(object):
         return beneficial_owners
 
 
-class AuthorizedUser(object):
+class AuthorizedUser(UnitDTO):
     def __init__(self, full_name: FullName, email: str, phone: Phone, jwt_subject: Optional[str]):
         self.full_name = full_name
         self.email = email
@@ -223,7 +223,7 @@ class AuthorizedUser(object):
         return [AuthorizedUser(FullName.from_json_api(d.get("fullName")), d.get("email"),
                               Phone.from_json_api(d.get("phone")), d.get("jwtSubject")) for d in data]
 
-class WireCounterparty(object):
+class WireCounterparty(UnitDTO):
     def __init__(self, routing_number: str, account_number: str, name: str, address: Address):
         self.routing_number = routing_number
         self.account_number = account_number
@@ -235,7 +235,7 @@ class WireCounterparty(object):
         return WireCounterparty(data["routingNumber"], data["accountNumber"], data["name"],
                                 Address.from_json_api(data["address"]))
 
-class Counterparty(object):
+class Counterparty(UnitDTO):
     def __init__(self, routing_number: str, account_number: str, account_type: str, name: str):
         self.routing_number = routing_number
         self.account_number = account_number
@@ -246,7 +246,7 @@ class Counterparty(object):
     def from_json_api(data: Dict):
         return Counterparty(data["routingNumber"], data["accountNumber"], data["accountType"], data["name"])
 
-class Coordinates(object):
+class Coordinates(UnitDTO):
     def __init__(self, longitude: int, latitude: int):
         self.longitude = longitude
         self.latitude = latitude
@@ -256,7 +256,7 @@ class Coordinates(object):
         return Coordinates(data["longitude"], data["latitude"])
 
 
-class Merchant(object):
+class Merchant(UnitDTO):
     def __init__(self, name: str, type: int, category: str, location: Optional[str]):
         self.name = name
         self.type = type
@@ -267,7 +267,7 @@ class Merchant(object):
     def from_json_api(data: Dict):
         return Merchant(data["name"], data["type"], data["category"], data.get("location"))
 
-class CardLevelLimits(object):
+class CardLevelLimits(UnitDTO):
     def __init__(self, daily_withdrawal: int, daily_purchase: int, monthly_withdrawal: int, monthly_purchase: int):
         self.daily_withdrawal = daily_withdrawal
         self.daily_purchase = daily_purchase
@@ -279,7 +279,7 @@ class CardLevelLimits(object):
         return CardLevelLimits(data["dailyWithdrawal"], data["dailyPurchase"], data["monthlyWithdrawal"],
                       data["monthlyPurchase"])
 
-class CardTotals(object):
+class CardTotals(UnitDTO):
     def __init__(self, withdrawals: int, deposits: int, purchases: int):
         self.withdrawals = withdrawals
         self.deposits = deposits
@@ -290,7 +290,7 @@ class CardTotals(object):
         return CardTotals(data["withdrawals"], data["deposits"], data["purchases"])
 
 
-class DeviceFingerprint(object):
+class DeviceFingerprint(UnitDTO):
     def __init__(self, value: str, provider: str = "iovation"):
         self.value = value
         self.provider = provider
