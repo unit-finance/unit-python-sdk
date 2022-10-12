3efbe69b8689f5c861377ced78da0a58b872c8b6
axshani <aalexshani@gmail.com>
2022-10-12 18:46:36 +0300

    jwtSubject renamed to jwtToken

--- e2e_tests/customerToken_test.py
+++ e2e_tests/customerToken_test.py
@@ -13,12 +13,14 @@ def test_create_token():
     response = client.customerTokens.create_token(request)
     assert response.data.type == "customerBearerToken"
 
+
 def test_create_jwt_token():
     account_id = create_individual_customer()
     request = CreateCustomerToken(account_id, "customers accounts",
-                                  jwt_subject="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9fQ")
+                                  jwt_token="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlZPV3VLR1R4eFlwRUNWSlAwdDRTNiJ9.eyJpc3MiOiJodHRwczovL2Rldi04NTRvbjk3dC51cy5hdXRoMC5jb20vIiwic3ViIjoiYkpjUkQ1SHo2eWRDWXlaTDVnTjE4MXo0OGxKamFOQURAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vZGV2LTg1NG9uOTd0LnVzLmF1dGgwLmNvbS9hcGkvdjIvIiwiaWF0IjoxNjQyOTMwNDA5LCJleHAiOjE2NDMwMTY4MDksImF6cCI6ImJKY1JENUh6NnlkQ1l5Wkw1Z04xODF6NDhsSmphTkFEIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.DCF_ODpiBPhn-fcr-FF3-Ayf6fq9g8UjSqTssfvdALNxZDQMGXaQeCIisy2NLrhF81PHAX3dZzcZbpeO93OlOAomXlaVzNomEd-pCLvjv1dQoQRc2BB3IMWUxbPBdGV7kztJzIfUwrOBTNV-DqSdB4SoGYROveFa3An4Mlj2FjArTXDXhnUytq15X5p_k4zLLNzznHVd-Tdcnd7hz9sA1vvtWXu")
     response = client.customerTokens.create_token(request)
     assert response.data.type == "customerBearerToken"
+
 
 def test_create_token_verification():
     account_id = create_individual_customer()
