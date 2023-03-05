5b949107600d24235306ffad18669ff79e39c2ed
Alex Shani <77099403+axshani@users.noreply.github.com>
2023-03-05 20:39:09 +0200

    typo fix (#171)
    added a test

--- e2e_tests/reward_test.py
+++ e2e_tests/reward_test.py
@@ -22,6 +22,21 @@ def test_create_reward():
     assert reward.data.type == "reward"
 
 
+def test_create_reward_with_funding_account():
+    account_id1 = create_deposit_account().data.id
+    account_id2 = create_deposit_account().data.id
+    req = CreateRewardRequest(3000, "Reward for transaction #5678", account_id1, funding_account_id=account_id2,
+                              tags={"test": "rewards"})
+    res = client.rewards.create(req)
+    assert res.data.type == "reward"
+    assert res.data.attributes["amount"] == 3000
+    assert res.data.attributes["description"] == "Reward for transaction #5678"
+
+    reward_id = res.data.id
+
+    reward = client.rewards.get(reward_id, "account, transaction, customer")
+    assert reward.data.type == "reward"
+
 def test_list_rewards():
     res = client.rewards.list()
     for r in res.data:
