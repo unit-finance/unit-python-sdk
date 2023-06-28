5c88a9f3c96f19c004f6b606864cdea7d6a45fbd
GermanSmoliar <german@unit.co>
2023-06-28 17:17:43 +0300

    Version 0.26.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='unit-python-sdk',
     packages=['unit', 'unit.api', 'unit.models', 'unit.utils'],
-    version="0.25.0",
+    version="0.26.0",
     license='Mozilla Public License 2.0',
     description='This library provides a python wrapper to http://unit.co API. See https://docs.unit.co/',
     author='unit.co',
