d25d2dcc0e7788a2f34cb97f1025c151a0d5ee96
Julia Park <julia@mazumago.com>
2023-08-17 15:38:13 -0400

    Add batch releases to Unit object

---
+++ unit/api/batch_release_resource.py
@@ -0,0 +1,17 @@
+from unit.api.base_resource import BaseResource
+from unit.models.batch_release import *
+from unit.models.codecs import DtoDecoder
+
+
+class BatchReleaseResource(BaseResource):
+    def __init__(self, api_url, token):
+        super().__init__(api_url, token)
+        self.resource = 'batch-releases'
+
+    def create(self, batch_releases: List[CreateBatchReleaseRequest]) -> Union[UnitResponse[List[BatchReleaseDTO]], UnitError]:
+        response = super().post(self.resource, {"data": batch_releases})
+        if super().is_20x(response.status_code):
+            data = response.json().get("data")
+            return UnitResponse[BatchReleaseDTO](DtoDecoder.decode(data), DtoDecoder.decode(data)) # is this right based on list??
+        else:
+            return UnitError.from_json_api(response.json())
---
+++ unit/models/batch_release.py
@@ -0,0 +1,57 @@
+from unit.models import *
+
+
+class BatchReleaseDTO(object):
+    def __init__(self, id: str, amount: int, description: str, sender_name: str, sender_address: Address,
+                 sender_account_number: str, tags: Optional[Dict[str, str]],
+                 relationships: Optional[Dict[str, Relationship]]):
+        self.id = id
+        self.type = "batchRelease"
+        self.attributes = {
+            "amount": amount,
+            "description": description,
+            "senderName": sender_name,
+            "senderAccountNumber": sender_account_number,
+            "senderAddress": sender_address,
+            "tags": tags,
+        }
+        self.relationships = relationships
+
+    @staticmethod
+    def from_json_api(_id, _type, attributes, relationships):
+        return BatchReleaseDTO(_id, attributes["amount"], attributes["description"], attributes["senderName"], attributes["senderAddress"], attributes["senderAccountNumber"], attributes.get("tags"), relationships)
+
+
+class CreateBatchReleaseRequest(object):
+    def __init__(self, amount: int, description: str, sender_name: str, sender_address: Address,
+                 sender_account_number: str, relationships: Optional[Dict[str, Relationship]], tags: Optional[Dict[str, str]] = None,
+                 idempotency_key: Optional[str] = None):
+        self.amount = amount
+        self.description = description
+        self.sender_name = sender_name
+        self.sender_address = sender_address
+        self.sender_account_number = sender_account_number
+        self.tags = tags
+        self.idempotency_key = idempotency_key
+        self.relationships = relationships
+
+    def to_json_api(self) -> Dict:
+        payload = {
+            "type": "batchRelease",
+            "attributes": {
+                "amount": self.amount,
+                "description": self.description,
+                "senderName": self.sender_name,
+                "senderAccountNumber": self.sender_account_number,
+                "senderAddress": self.sender_address
+            },
+            "relationships": self.relationships
+        }
+
+        if self.idempotency_key:
+            payload["data"]["attributes"]["idempotencyKey"] = self.idempotency_key
+
+        if self.tags:
+            payload["data"]["attributes"]["tags"] = self.tags
+
+        return payload
