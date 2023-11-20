f24a102235401645ad2befa082def780cfcdc1ea
Alex Shani <77099403+axshani@users.noreply.github.com>
2023-11-20 13:15:09 +0200

    update attributes (#249)
    update applicant_details field
    update applicationForm_test.py

--- e2e_tests/applicationForm_test.py
+++ e2e_tests/applicationForm_test.py
@@ -2,16 +2,33 @@ import os
 
 from e2e_tests.helpers.helpers import create_individual_application
 from unit import Unit
+from unit.models import FullName, BusinessContact, Phone, Officer, Address, BeneficialOwner
 from unit.models.applicationForm import CreateApplicationFormRequest, ApplicationFormPrefill
 
 token = os.environ.get('TOKEN')
 client = Unit("https://api.s.unit.sh", token)
 card_types = ["individualDebitCard", "businessDebitCard", "individualVirtualDebitCard", "businessVirtualDebitCard"]
 
+
 def create_application_form():
+    ad = ApplicationFormPrefill("Business", FullName("Peter", "Parker"), "721074426", "12345678", "US", "2001-08-10",
+                           "peter@oscorp.com", "Pied Piper", "DE", "Corporation",
+                           BusinessContact(FullName("Richard", "Hendricks"), "richard@piedpiper.com",
+                                           Phone("1", "5555555555")),
+                           Officer(FullName("Richard", "Hendricks"), "2001-08-10",
+                                   Address("5230 Newell Rd", "Palo Alto",  "CA", "94303", "US"),
+                                   Phone("1", "5555555555"), "richard@piedpiper.com", title="COO", ssn="721074426"),
+                           [BeneficialOwner(FullName("Richard", "Hendricks"), "2001-08-10",
+                                            Address("5230 Newell Rd", "Palo Alto",  "CA", "94303", "US"),
+                                            Phone("1", "5555555555"), "richard@piedpiper.com", ssn="123456789",
+                                            percentage=75)], "https://www.piedpiper.com", "Piedpiper Inc", "123456789",
+                           Address("5230 Newell Rd", "Palo Alto",  "CA", "94303", "US"), Phone("1", "5555555555"),
+                           "Doctor", "Between10kAnd25k", "EmploymentOrPayrollIncome", "TechnologyMediaOrTelecom",
+                           "Between500kAnd1m", "Between50And100", "Predictable", "2014", ["US", "CA"], "PPI", False)
+
     request = CreateApplicationFormRequest(tags={"userId": "106a75e9-de77-4e25-9561-faffe59d7814"},
                                            allowed_application_types=["Individual"],
-                                           application_details={"jwtSubject": "test4"})
+                                           applicant_details=ad)
 
     return client.applicationForms.create(request)
 
@@ -20,7 +37,7 @@ def create_application_form_with_prefill():
     prefill = ApplicationFormPrefill(email="test@castlepay.co")
 
     request = CreateApplicationFormRequest(tags={"userId": "106a75e9-de77-4e25-9561-faffe59d7814"},
-                                           application_details=prefill)
+                                           applicant_details=prefill)
 
     return client.applicationForms.create(request)
 
