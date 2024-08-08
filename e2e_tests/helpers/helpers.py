4537468ae56dedc9b70a7a7fa78b29b6256a322f
Ilya Merman <ilya.merman@gmail.com>
2024-08-08 11:58:28 +0300

    Remove trust (#283)

--- e2e_tests/helpers/helpers.py
+++ e2e_tests/helpers/helpers.py
@@ -2,8 +2,7 @@ import uuid
 
 from typing import List
 from datetime import date, timedelta
-from unit.models import Relationship, Address, Phone, FullName, Counterparty, WireCounterparty, Grantor, TrustContact,\
-    Trustee, Beneficiary
+from unit.models import Relationship, Address, Phone, FullName, Counterparty, WireCounterparty
 from unit.models.account import CreateDepositAccountRequest
 from unit.models.application import CreateIndividualApplicationRequest
 
@@ -86,89 +85,3 @@ def create_deposit_account(client):
                                           {"customer": Relationship("customer", customer_id)},
                                           {"purpose": "credit_operating"})
     return client.accounts.create(request)
-
-
-def create_grantor():
-    return Grantor.from_json_api({
-                                                "fullName": {
-                                                    "first": "Laurie",
-                                                    "last": "Bream"
-                                                },
-                                                "dateOfBirth": "2000-01-01",
-                                                "ssn": "000000003",
-                                                "email": "laurie@raviga.com",
-                                                "phone": {
-                                                    "countryCode": "1",
-                                                    "number": "2025550108"
-                                                },
-                                                "address": {
-                                                    "street": "950 Allerton Street",
-                                                    "city": "Redwood City",
-                                                    "state": "CA",
-                                                    "postalCode": "94063",
-                                                    "country": "US"
-                                                }
-                                            })
-
-
-def create_trust_contact():
-    return TrustContact.from_json_api({
-        "fullName": {
-          "first": "Jared",
-          "last": "Dunn"
-        },
-        "email": "jared@piedpiper.com",
-        "phone": {
-          "countryCode": "1",
-          "number": "2025550108"
-        },
-        "address": {
-          "street": "5230 Newell Rd",
-          "city": "Palo Alto",
-          "state": "CA",
-          "postalCode": "94303",
-          "country": "US"
-        }
-      })
-
-
-def create_trustee():
-    return [Trustee.from_json_api({
-          "fullName": {
-            "first": "Richard",
-            "last": "Hendricks"
-          },
-          "dateOfBirth": "2000-01-01",
-          "ssn": "000000002",
-          "email": "richard@piedpiper.com",
-          "phone": {
-            "countryCode": "1",
-            "number": "2025550108"
-          },
-          "address": {
-            "street": "5230 Newell Rd",
-            "city": "Palo Alto",
-            "state": "CA",
-            "postalCode": "94303",
-            "country": "US"
-          }
-        })]
-
-
-def create_beneficiaries():
-    return [
-        Beneficiary.from_json_api({
-          "fullName": {
-            "first": "Dinesh",
-            "last": "Chugtai"
-          },
-          "dateOfBirth": "2000-01-01"
-        }),
-        Beneficiary.from_json_api({
-          "fullName": {
-            "first": "Gilfoyle",
-            "last": "Unknown"
-          },
-          "dateOfBirth": "2000-01-01"
-        })
-    ]
